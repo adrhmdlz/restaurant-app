60c785fae6145ffccc81bf727f49b6253c7cfaf7
adrhmdlz <adrianalzidan35@gmail.com>
2024-02-19 11:06:22 +0700

    add: Added new file for Hero components (AppHero.js & _appHero.scss).

--- src/scripts/components/AppHero.js
+++ src/scripts/components/AppHero.js
@@ -10,7 +10,8 @@ class AppHero extends HTMLElement {
   render() {
     this.innerHTML = `
       <div class="hero-wrapper">
-        <h1>Mau makan dimana kita hari ini?</h2>
+        <h1>Mau makan dimana hari ini?</h2>
+        <a href="#">Cari Restaurant</a>
       </div>
     `;
   }
