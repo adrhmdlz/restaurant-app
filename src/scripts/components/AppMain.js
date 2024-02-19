cd862e6f26e8ef0f02bbf1807ca148552fcc3714
adrhmdlz <adrianalzidan35@gmail.com>
2024-02-19 17:47:31 +0700

    mod: Added new element and styling for main section (AppMain.js, _appMain.scss).

--- src/scripts/components/AppMain.js
+++ src/scripts/components/AppMain.js
@@ -1,3 +1,5 @@
+const { restaurants } = require('../../public/data/DATA.json');
+
 class AppMain extends HTMLElement {
   constructor() {
     super();
@@ -8,9 +10,39 @@ class AppMain extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `
-      <h2>Hello, World</h2>
+    // Membuat string kosong untuk menyimpan HTML yang akan dirender
+    let html = `
+      <div class="main-wrapper">
+        <h2 class="main-header" tabindex="0">Rekomendasi Restaurant</h2>
+        <div class="card-wrapper">
     `;
+
+    // Iterasi pada setiap objek dalam array dan menambahkan HTML untuk setiap objek
+    restaurants.forEach((item) => {
+      html += `
+          <div class="card">
+            <img src="${item.pictureId}" alt="${item.name}" title="${item.name}" class="card-image"></img>
+            <div class="card-body">
+              <div class="card-header">
+                <h3>${item.name}</h3>
+                <i class="fas fa-star star">
+                  <p>${item.rating}</p>
+                </i>
+              </div>
+              <p class="card-desc">${item.description}</p>
+            </div>
+          </div>
+      `;
+    });
+
+    // Menutup tag div
+    html += `
+        </div>
+      </div>
+    `;
+
+    // Mengatur innerHTML dari custom element dengan HTML yang telah dibuat
+    this.innerHTML = html;
   }
 }
 
