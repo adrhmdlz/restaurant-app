c974267bd1537e7daa679262bea7b93625b24c7d
adrhmdlz <adrianalzidan35@gmail.com>
2024-03-11 19:08:23 +0700

    add: Added new file api endpoint configuration (api-endpoint.js).

---
+++ src/scripts/global/api-endpoint.js
@@ -0,0 +1,11 @@
+import CONFIG from './config';
+
+const API_ENDPOINT = {
+  LIST_RESTAURANT: `${CONFIG.BASE_URL}/list`,
+  RESTAURANT_IMAGE_URL: `${CONFIG.BASE_URL}/images/medium`,
+  ADD_REVIEW: `${CONFIG.BASE_URL}/review`,
+  DETAIL: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
+  SEARCH_RESTAURANT: (query) => `${CONFIG.BASE_URL}/search?q=${query}`,
+};
+
+export default API_ENDPOINT;
