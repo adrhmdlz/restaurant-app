ae7722305d3372782fbce11f5527dbc58e373c06
adrhmdlz <adrianalzidan35@gmail.com>
2024-03-11 19:08:53 +0700

    add: Added new file for configuration (config.js).

---
+++ src/scripts/global/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev',
+  DATABASE_NAME: 'restaurant-catalogue-database',
+  OBJECT_STORE_NAME: 'restaurants',
+  DATABASE_VERSION: 1,
+};
+
+export default CONFIG;
